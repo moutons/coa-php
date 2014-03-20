e33bf28b5d2079859969d33a00b519e1ae599d5c
Jerald Sheets <questy@gmail.com>
2014-03-20 17:07:51 -0500

    reverted to original class loading mechanism

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 # CoA PHP module
 class coa-php {
-  class { '::coa-php::config': } ->
-  class { '::coa-php::install': } ->
-  Class['coa-php']
+  include coa-php::config
+  include coa-php::install
 }
